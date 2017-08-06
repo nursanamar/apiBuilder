1997dee7747ccfcb8dd97f74d816dfe681f8b269
Nursan amar <nursan011@gmail.com>
2017-08-06 01:42:05 +0000

    fix index

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,24 +21,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function __construct() {
 		parent::__construct();
-		$this->load->model("model");
 	}
 	public function index()
 	{
-		//$this->load->view('welcome_message');
-		$data = $this->model->ambil();
-		$json = json_encode($data->result_array());
-		echo $json;
-	}
-	public function table() {
-		$this->load->view("home");
-		print_r($_GET);
-	}
-	public function tambah($nama,$kelas){
-		//$nama ="nama";// $this->input->post['nama'];
-		//$kelas = "kelas";//$this->input->post['kelas'];
-		$data = array("nama" => $nama,"kelas" => $kelas);
-		$this->model->tambah($data);
-		echo "berhasil";
+		echo "deploy";
 	}
 }
